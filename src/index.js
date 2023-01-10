080557276387b3caf2cb86811215faaabd322c14
choikwm <choikwm@gmail.com>
2023-01-11 00:13:47 +0800

    Fix change page scroll to top

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ScrollToTop from "./components/ScrollToTop";
 
 // import HomePage from "./pages/HomePage";
 // import AboutUsPage from "./pages/AboutUsPage";
@@ -13,6 +14,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
+        <ScrollToTop />
       <App />
     </BrowserRouter>
   </React.StrictMode>
